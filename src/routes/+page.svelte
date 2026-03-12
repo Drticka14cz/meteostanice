a452bede8e50f13e78529d178bc3500a2fefb0f3
Jakub Štěpánek <jakub14step@seznam.cz>
2026-03-12 15:21:49 +0100

    12.3. odpoledne

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,36 @@
 <div class="main">
     <div class="uvod"><div class="uvod_logo"><img src="logo_meteostanice.png" alt="logo meoteostanice" id="logo_meteostanice"></div></div>
-    <div class="mid">Pica</div>
+    <div class="mid"></div>
 </div>
 <style>
-    .uvod{
-        background-image:url("meteo_pozadi.png");
-        background-size: cover;
-        background-repeat: repeat;
-        background-position: center;
-        height: 100vh;
-        width: 100vw;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        animation: mraky_repeat infinite timing-function delay iteration-count direction fill-mode;
+    .uvod {
+    background-image: url("/meteo_pozadi.png");
+    background-size: cover;
+    background-repeat: repeat-x;
+    background-position: 0 0;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    animation: posun 30s linear infinite;
+    background-color: #1d71b8;
+}
+
+@keyframes posun {
+    from {
+        background-position:0;
     }
+    to {
+        background-position:100vw ;
+    }
+}
+
     .uvod_logo{
         height: 40vh;
         width: 40vw;
-        border: red dashed 2px;
+        /* border: red dashed 2px; */
 
     }
     #logo_meteostanice{
@@ -29,5 +40,6 @@
     .mid{
         min-height: 100vh;
     }
+
 
 </style>
